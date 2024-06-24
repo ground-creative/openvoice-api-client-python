cf8279c79a45b4611e701e4464f59bd73ff4bcca
Carlo Pietrobattista <irony00100@gmail.com>
2024-06-24 13:30:57 +0700

    first commit

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
+from openvoice_api_client import __version__
+
 setup(
     name='openvoice_api_client',
-    version='1.0.0',
+    version=__version__,
     description='OpenVoice API client',
-    url='https://github.com/yourusername/mypackage',
+    url='https://github.com/ground-creative/openvoice-api-client-python',
     author='Carlo Pietrobattista',
     author_email='irony00100@gmail.com',
     license='MIT',
+    py_modules=['openvoice_api_client'],
     packages=find_packages(),
     install_requires=[
         'requests',
